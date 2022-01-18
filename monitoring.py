2ae6e0c871bbe1f8c2194d18a3cd713105186a76
betterty <indexspo@mail.ru>
2022-01-18 18:49:10 +0300

    commit

--- monitoring.py
+++ monitoring.py
@@ -1,6 +1,7 @@
-import sys
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QTextBrowser, QMessageBox
-from PyQt5 import QtCore, QtWidgets,QtGui
+from sys import executable,argv,exit
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QTextEdit, QMessageBox, QLineEdit, QLabel
+from PyQt5 import QtCore
+from PyQt5.QtGui import QCloseEvent, QFont
 import os
 import subprocess
 import threading
@@ -13,7 +14,7 @@ class Example(QWidget):
         self.status = True
         self.initUI()
 
-    def closeEvent(self, a0: QtGui.QCloseEvent):  # close event
+    def closeEvent(self, a0: QCloseEvent):  # close event
         pipe_glob.kill()
         a0.accept()
 
@@ -26,7 +27,7 @@ class Example(QWidget):
                                 "QPushButton:pressed { background-color: red }")
         self.setGeometry(100, 100, 600, 575)
         self.setWindowTitle('Quit button')
-        self.lineEdit_1 = QtWidgets.QLineEdit(self)
+        self.lineEdit_1 = QLineEdit(self)
         self.lineEdit_1.setGeometry(QtCore.QRect(75, 70, 180, 30))
         self.lineEdit_1.setStyleSheet("background: rgb(255, 255, 255);")
         self.lineEdit_1.setObjectName("PORT")
@@ -35,7 +36,7 @@ class Example(QWidget):
         self.lineEdit_1.setFont(font)
         self.lineEdit_1.setAlignment(QtCore.Qt.AlignCenter)
         self.lineEdit_1.setText("COM5")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self)
+        self.lineEdit_2 = QLineEdit(self)
         self.lineEdit_2.setGeometry(QtCore.QRect(325, 70, 180, 30))
         self.lineEdit_2.setStyleSheet("background: rgb(255, 255, 255);")
         self.lineEdit_2.setObjectName("CONNECTION_SPEED")
@@ -44,7 +45,7 @@ class Example(QWidget):
         self.lineEdit_2.setFont(font)
         self.lineEdit_2.setAlignment(QtCore.Qt.AlignCenter)
         self.lineEdit_2.setText("9600")
-        self.lineEdit_3 = QtWidgets.QLineEdit(self)
+        self.lineEdit_3 = QLineEdit(self)
         self.lineEdit_3.setGeometry(QtCore.QRect(75, 170, 180, 30))
         self.lineEdit_3.setStyleSheet("background: rgb(255, 255, 255);")
         self.lineEdit_3.setObjectName("TIMEOUT")
@@ -53,43 +54,43 @@ class Example(QWidget):
         self.lineEdit_3.setFont(font)
         self.lineEdit_3.setAlignment(QtCore.Qt.AlignCenter)
         self.lineEdit_3.setText("1")
-        self.label_1 = QtWidgets.QLabel(self)
+        self.label_1 = QLabel(self)
         self.label_1.setGeometry(QtCore.QRect(150, 40, 70, 30))
         self.label_1.setObjectName("label_1")
-        font = QtGui.QFont()
+        font = QFont()
         font.setPointSize(11)
         font.setBold(False)
         font.setWeight(55)
         self.label_1.setFont(font)
         self.label_1.setObjectName("label_1")
         self.label_1.setText("Port:")
-        self.label_2 = QtWidgets.QLabel(self)
+        self.label_2 = QLabel(self)
         self.label_2.setGeometry(QtCore.QRect(355, 40, 150, 30))
         self.label_2.setObjectName("label_1")
-        font = QtGui.QFont()
+        font = QFont()
         font.setPointSize(11)
         font.setBold(False)
         font.setWeight(55)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_1")
         self.label_2.setText("Connection speed:")
-        self.label_3 = QtWidgets.QLabel(self)
+        self.label_3 = QLabel(self)
         self.label_3.setGeometry(QtCore.QRect(136, 140, 70, 30))
         self.label_3.setObjectName("label_1")
-        font = QtGui.QFont()
+        font = QFont()
         font.setPointSize(11)
         font.setBold(False)
         font.setWeight(55)
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_1")
         self.label_3.setText("Timeout:")
-        self.Text = QTextBrowser(self)
+        self.Text = QTextEdit(self)
         self.Text.setGeometry(75, 250, 430, 275)
         self.qbtn.clicked.connect(self.conductor)
-        self.label_4 = QtWidgets.QLabel(self)
+        self.label_4 = QLabel(self)
         self.label_4.setGeometry(QtCore.QRect(120, 533, 400, 30))
         self.label_4.setObjectName("label_1")
-        font = QtGui.QFont()
+        font = QFont()
         font.setPointSize(11)
         font.setBold(False)
         font.setWeight(55)
@@ -133,7 +134,7 @@ class Example(QWidget):
             connection_speed = self.lineEdit_2.text()
             timeout = self.lineEdit_3.text()
             plot = os.path.join(os.path.dirname(__file__), "./other.py")
-            path = [sys.executable, plot]
+            path = [executable, plot]
             pipe = subprocess.Popen(path, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             pipe.stdin.write((port+" ").encode("utf8"))
             pipe.stdin.write((connection_speed+" ").encode("utf8"))
@@ -152,8 +153,8 @@ class Example(QWidget):
                               "QPushButton:pressed { background-color: rgb(0, 255,0) }")
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication(argv)
     ex = Example()
     ex.setWindowTitle("Lebedev")
     ex.show()
-    sys.exit(app.exec_())
+    exit(app.exec_())
